8434f0b9983f5fa045e179bb9c7c81413115047f
chuvashovvigor-cyber <chuvashovv.igor@gmail.com>
2025-10-16 01:18:55 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from 'next'
-import './globals.css'
+import '../styles/globals.css'
 
 export const metadata: Metadata = {
-  title: 'Делов‑на‑час',
-  description: 'Фикс‑цены, быстрые заявки, подписки для физ и юр лиц. Живой трекинг мастера — как в такси.',
+  title: 'Делов-на-час',
+  description:
+    'Фикс-цены, быстрые заявки, подписки для физ и юр лиц. Живой трекинг мастера — как в такси.',
   icons: [{ rel: 'icon', url: '/favicon.ico' }],
 }
 
