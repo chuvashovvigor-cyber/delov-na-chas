9429271b5ea9a54db904a288d535d6fdf901a693
chuvashovvigor-cyber <chuvashovv.igor@gmail.com>
2025-10-26 01:04:21 +0300

    Update MapWithMasters.tsx

--- components/map/MapWithMasters.tsx
+++ components/map/MapWithMasters.tsx
@@ -8,7 +8,7 @@ import { useMemo } from 'react';
 
 type Props = { height?: string };
 
-const defaultIcon = new L.Icon({
+const masterIcon = new L.Icon({
   iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
   iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
   shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
@@ -17,16 +17,16 @@ const defaultIcon = new L.Icon({
 });
 
 export default function MapWithMasters({ height = '360px' }: Props) {
-  // Калуга
+  // Центр Калуги
   const center = useMemo<[number, number]>(() => [54.513845, 36.261215], []);
   return (
     <div className="w-full rounded-xl overflow-hidden border border-zinc-800">
-      <MapContainer center={center} zoom={14} style={{ height }} scrollWheelZoom className="bg-zinc-900">
+      <MapContainer center={center} zoom={14} style={{ height }} scrollWheelZoom>
         <TileLayer
           attribution="&copy; OpenStreetMap &copy; CARTO"
           url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
         />
-        <Marker position={center} icon={defaultIcon}>
+        <Marker position={center} icon={masterIcon}>
           <Popup>Мастер рядом</Popup>
         </Marker>
       </MapContainer>
