b516948ca6f79ff6dd010de9c7ded53cf18960b5
chuvashovvigor-cyber <chuvashovv.igor@gmail.com>
2025-11-15 02:34:35 +0300

    Create Box.tsx

---
+++ components/Box.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Box = (): JSX.Element => {
+  return (
+    <div className="w-[590px] h-[980px] relative">
+      <img
+        className="absolute w-full h-full top-0 left-0 object-cover"
+        alt="Delov-na-chas"
+        src="/landing.svg" // ← то имя, которое ты дал файлу
+      />
+    </div>
+  );
+};
+
+export default Box;
