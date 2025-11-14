de08fe2f6db07188e78ed6980e1a8bf7502d682a
chuvashovvigor-cyber <chuvashovv.igor@gmail.com>
2025-11-15 02:48:01 +0300

    Update Box.tsx

--- app/components/Box.tsx
+++ app/components/Box.tsx
@@ -3,13 +3,11 @@ import React from "react";
 
 const Box = (): JSX.Element => {
   return (
-    <div className="w-[590px] h-[980px] relative">
-      <img
-        className="absolute w-full h-full top-0 left-0 object-cover"
-        alt="Delov-na-chas"
-        src="/landing.svg" // ← здесь имя файла из public
-      />
-    </div>
+    <img
+      src="/landing.svg" // <-- тут имя файла из public (может быть другое)
+      alt="Delov-na-chas экран"
+      className="block w-full max-w-[390px] h-auto mx-auto rounded-3xl shadow-2xl"
+    />
   );
 };
 
